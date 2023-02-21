7cbedfd26abf7e5eaa4251fcb9b95c72482aff85
ShivangiGupta123 <ShivangiGupta123>
2023-02-21 15:25:28 +0530

    worked on Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
-import {Route , Routes , BrowserRouter} from 'react-router-dom'
-import Dashboard from './component/user/dashboard/Dashboard';
-import Event_form from './component/user/event_form/Event_form';
-import List_of_participant from './component/user/list_of_participant/List_of_participant';
-import Show_result from './component/user/Result/Show_result';
-import Result_form from './component/user/result_form/Result_form';
-
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Public_layout from "./component/layout/Public_layout";
+import Dashboard from "./component/user/dashboard/Dashboard";
+import Event_form from "./component/user/event_form/Event_form";
+import List_of_participant from "./component/user/list_of_participant/List_of_participant";
+import Show_result from "./component/user/Result/Show_result";
+import Result_form from "./component/user/result_form/Result_form";
 
 function App() {
   return (
     <div className="App">
-   
-
-    <BrowserRouter>
-    <Dashboard/>
-    <Routes>
-    
-    <Route path = "/eventform" element = {<Event_form/>}/>
-    <Route path = "/listofparticipant" element = {<List_of_participant/>}/>
-    <Route path = "/resultform" element = {<Result_form/>}/>
-    <Route path = "/showresult" element = {<Show_result/>}/>
-    
-    </Routes>
-    </BrowserRouter>
-   
+      <BrowserRouter>
+        <Dashboard />
+        <Routes>
+          <Route element={<Public_layout />}>
+            <Route path="/eventform" element={<Event_form />} />
+            <Route
+              path="/listofparticipant"
+              element={<List_of_participant />}
+            />
+            <Route path="/resultform" element={<Result_form />} />
+            <Route path="/showresult" element={<Show_result />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/component/layout/Public_layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+function Public_layout() {
+  return (
+    <div>
+    
+    <Outlet/>
+    
+    
+    </div>
+  )
+}
+
+export default Public_layout
